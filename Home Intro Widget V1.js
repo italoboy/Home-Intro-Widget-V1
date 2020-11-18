a898fb81891588103ad0d9c17a915bea6a33228e
italoboy <61342502+italoboy@users.noreply.github.com>
2020-11-18 08:14:09 +0330

    Update Home Intro Widget V1.js

--- Home Intro Widget V1.js
+++ Home Intro Widget V1.js
@@ -89,7 +89,7 @@ async function createWidget() {
 	//const dateString = `${formatDate('EEEE, MMM dd',today)}${nth(today.getDate())}`
 	const dateString = formatDate('EEEE dd MMMM',today)
 	const datetext = hStack.addText(dateString);
-	datetext.font = Font.boldSystemFont(19);
+	datetext.font = Font.boldSystemFont(20);
 	datetext.textColor = themeColor;
 	datetext.textOpacity = (0.5);
 	datetext.leftAlignText();
